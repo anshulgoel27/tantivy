e3023d1682244d1d997e730c4c3901f57f2ca791
anshulgoel27 <anshulgoel27@gmail.com>
2024-10-02 12:20:48 +0530

    updated B value to 0.3

--- src/query/bm25.rs
+++ src/query/bm25.rs
@@ -6,7 +6,7 @@ use crate::schema::Field;
 use crate::{Score, Searcher, Term};
 
 const K1: Score = 1.2;
-const B: Score = 0.0;
+const B: Score = 0.3;
 
 /// An interface to compute the statistics needed in BM25 scoring.
 ///
